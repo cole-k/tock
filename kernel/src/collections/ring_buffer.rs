65478d9066fdaba8d1862e4484b7d3402696c104
Cole K <cole-k@users.noreply.github.com>
2025-02-17 17:33:51 -0800

    refine enqueue with invalid refinement

--- kernel/src/collections/ring_buffer.rs
+++ kernel/src/collections/ring_buffer.rs
@@ -94,16 +94,16 @@ impl<T: Copy> queue::Queue<T> for RingBuffer<'_, T> {
         }
     }
 
-    #[flux_rs::sig(fn(self: &strg RingBuffer<T>[@old], val: T) -> bool{ b: b == !full(old) }
+    #[flux_rs::sig(fn(self: &strg RingBuffer<T>[@old], val: T) -> bool[!full(old)]
                    ensures self: RingBuffer<T>{new :
-                     if full(old) { old.hd == new.hd && old.tl == new.tl } else { true }
+                     if full(old) {
+                         old == new
+                     } else {
+                         new.ring_len == old.ring_len && new.hd == next_hd(old)
+                         && new.tl == next_tl(old)
+                     }
                    }
     )]
-    //
-    //
-    //
-    //
-    //
     fn enqueue(&mut self, val: T) -> bool {
         if self.is_full() {
             // Incrementing tail will overwrite head
